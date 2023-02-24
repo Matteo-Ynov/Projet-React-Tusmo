fc653e73253ec19f21789935f04d49b0c459deff
Mattéo Christophe <matteo.christophe@opquast.com>
2023-02-24 10:19:46 +0100

    kinda working game

---
+++ src/engine.js
@@ -0,0 +1,71 @@
+export function getHints(word, input) {
+    let [correct_letters, changed_word] = checkCorrectLetters(word, input);
+    let misplaced_letters = checkMisplacedLetters(changed_word, input);
+    return joinCorrectAndMisplacedLetters(
+        input,
+        correct_letters,
+        misplaced_letters
+    );
+}
+
+function checkCorrectLetters(word, input) {
+    word = word.split("");
+    input = input.split("");
+    let correct_letters = [];
+    for (let i = 0; i < word.length; i++) {
+        if (input[i] === word[i]) {
+            correct_letters.push(true);
+            word[i] = "#";
+        } else {
+            correct_letters.push(false);
+        }
+    }
+    return [correct_letters, word.join()];
+}
+
+function checkMisplacedLetters(word, input) {
+    word = word.split("");
+    input = input.split("");
+    let misplaced_letters = [];
+    for (let i = 0; i < word.length; i++) {
+        if (word.includes(input[i])) {
+            misplaced_letters.push(true);
+            var index = word.indexOf(input[i]);
+            word.splice(index, 1);
+        } else {
+            misplaced_letters.push(false);
+        }
+    }
+    return misplaced_letters;
+}
+
+function joinCorrectAndMisplacedLetters(
+    input,
+    correct_letters,
+    misplaced_letters
+) {
+    let list = [];
+    input = input.split("");
+    for (let i = 0; i < input.length; i++) {
+        if (correct_letters[i] === true) {
+            list.push((input[i], "well-placed"));
+        } else if (misplaced_letters[i] === true) {
+            list.push((input[i], "misplaced"));
+        } else {
+            list.push((input[i], ""));
+        }
+    }
+    console.log(list);
+    return list;
+}
+
+export const checkIfWordExist = (input) => {
+    return new Promise((resolve, reject) => {
+        fetch(
+            `https://frenchwordsapi.herokuapp.com/api/Word?idOrName=${input}`
+        ).then((response) => {
+            const status = response["status"];
+            status === 200 ? resolve(true) : resolve(false);
+        });
+    });
+};
---
+++ src/states/wordsReducer.js
@@ -0,0 +1,88 @@
+import wordList from "../assets/words.json";
+import { getHints } from "../engine.js";
+
+export const SET_WORD_TO_FIND = "SET_WORD_TO_FIND";
+export const TYPE = "TYPE";
+export const RESET = "RESET";
+
+const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+var lengthWord = 6;
+var words = wordList[lengthWord];
+
+const generateNewState = () => {
+    var choosenWord = words[Math.floor(Math.random() * words.length)]
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .toUpperCase();
+    return {
+        wordToFind: choosenWord,
+        tries: [],
+        hints: [],
+        currentTry: "",
+        letterPlacement: choosenWord[0] + ".".repeat(choosenWord.length - 1),
+        wordLength: choosenWord.length,
+        won: undefined,
+    };
+};
+
+export const initialState = generateNewState();
+
+export const wordsReducer = (state, action) => {
+    switch (action.type) {
+        case RESET:
+            return generateNewState();
+        case SET_WORD_TO_FIND:
+            return {
+                ...state,
+                wordToFind: action.payload,
+            };
+        case TYPE:
+            if (
+                ALPHABET.includes(action.payload) &&
+                state.currentTry.length < state.wordLength
+            ) {
+                if (
+                    state.currentTry.length === 0 &&
+                    action.payload.toUpperCase() !== state.wordToFind[0]
+                ) {
+                    state.currentTry += state.wordToFind[0];
+                    state.currentTry += action.payload.toUpperCase();
+                } else {
+                    state.currentTry += action.payload.toUpperCase();
+                }
+            } else if (
+                action.payload === "backspace" &&
+                state.currentTry.length > 0
+            ) {
+                state.currentTry = state.currentTry.slice(0, -1);
+            } else if (
+                action.payload === "enter" &&
+                state.currentTry.length === state.wordLength
+            ) {
+                state.tries.push(state.currentTry);
+                state.hints.push(getHints(state.wordToFind, state.currentTry));
+                for (let i = 0; i < state.hints[state.hints.length - 1].length; i++) {
+                    if (state.hints[state.hints.length - 1][i] == "well-placed") {
+                        state.letterPlacement = state.letterPlacement.split("");
+                        state.letterPlacement[i] = state.wordToFind[i];
+                        state.letterPlacement = state.letterPlacement.join("");
+                    }
+                }
+                if (state.currentTry === state.wordToFind) {
+                    state.won = true;
+                } else if (state.tries.length === 6) {
+                    state.won = false;
+                }
+                console.log(state.won);
+                state.currentTry = "";
+            }
+
+            return {
+                ...state,
+            };
+
+        default:
+            return state;
+    }
+};
